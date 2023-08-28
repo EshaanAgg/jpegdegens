8b7e4e241609774011611b9c6fd54a7e5a167728
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-08-28 13:35:37 +0530

    Add Webpack to connect to MetaMask

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
-import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-waffle";
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.19",
